bb170073e4681fc577c08a2c92af2881a215d2ae
PranavSuresh <sureshpranav2001@gmail.com>
2015-08-11 12:06:43 -0400

    Update _1Blink_2014.ino

--- Example Programs/_1Blink_2014/_1Blink_2014.ino
+++ Example Programs/_1Blink_2014/_1Blink_2014.ino
@@ -1,13 +1,16 @@
 //one color
 int led = 3;     //Declares Var led to pin 3
+int x=0;
 void setup()
 {
   pinMode(led, OUTPUT);    //Declares pin 3 to be an output   
 }
-for(x, x < 10, x++)   //repeats while x is < 10 x goes up by a constant 1
+void loop(){
+ //for(x, x < 10, x++)   //repeats while x is < 10 x goes up by a constant 1
 {
   digitalWrite(led, HIGH); //sets led on HIGH
-  delay(1000);    //waits 1 sec
+  delay(100);    //waits 1 sec
   digitalWrite(led, LOW);    //sets led to LOW
-  delay(1000);   //waits 1 sec
+  delay(100);   //waits 1 sec
+}
 }
