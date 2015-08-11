c0235873562a204bf75c995a857f749ec09592f3
PranavSuresh <sureshpranav2001@gmail.com>
2015-08-11 12:15:04 -0400

    Update _1Blink_2014.ino

--- Example Programs/_1Blink_2014/_1Blink_2014.ino
+++ Example Programs/_1Blink_2014/_1Blink_2014.ino
@@ -1,16 +1,16 @@
-//one color
-int led = 3;     //Declares Var led to pin 3
-int x=0;
-void setup()
-{
-  pinMode(led, OUTPUT);    //Declares pin 3 to be an output   
+// the setup routine runs once when you press reset:
+void setup() {                
+  // initialize the digital pin as an output.
+  pinMode(0, OUTPUT); //LED on Model B
+  pinMode(1, OUTPUT); //LED on Model A  or Pro
 }
-void loop(){
- //for(x, x < 10, x++)   //repeats while x is < 10 x goes up by a constant 1
-{
-  digitalWrite(led, HIGH); //sets led on HIGH
-  delay(100);    //waits 1 sec
-  digitalWrite(led, LOW);    //sets led to LOW
-  delay(100);   //waits 1 sec
-}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  digitalWrite(0, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(1, HIGH);
+  delay(1000);               // wait for a second
+  digitalWrite(0, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(1, LOW); 
+  delay(1000);               // wait for a second
 }
