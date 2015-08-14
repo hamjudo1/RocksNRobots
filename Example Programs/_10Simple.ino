60e7d3472f04bf478ca4c7ce69f70dbf834838e1
Paul Haas <hamjudo1@users.noreply.github.com>
2015-08-13 22:45:36 -0400

    Update _10Simple.ino
    Cleaned up code, should be functionally equivalent to the last version.

--- Example Programs/_10Simple.ino
+++ Example Programs/_10Simple.ino
@@ -2,19 +2,16 @@
  is in math. In programming, a variable is a word with
  just use the number 0, instead.
  */
-// ******* Different naming conventions between analog to digital signals
-// ******* Investigate variables being defined as const vs int
-// ******* should the declarations be in void setup or above?
 
 #include "rnr.h"
 const int motor1basespeed = 260;
 const int motor2basespeed = 220;
-const int led = 3;
-const int motor1 = 0;      // motor1 connected to analog pin 7?
-const int motor2 = 1;      // motor1 connected to analog pin 8?
-int ALTIMETER_PIN = 1;  //Downsensor pin location
+const int led = 3;  // LED is on digital pin 3
+const int motor1 = 0; // First motor on digital pin 0
+const int motor2 = 1;    // Second motor on digital pin 1
+int ALTIMETER_PIN = 1;  //Downsensor analog pin location (analog pin 1, is different from digital pin 1!) 
 int FRONT_SENSOR_PIN = 2;  //Frontsensor pin location
-int GYRO_SENSOR_PIN = 0;  // Input signal for gyro value
+int GYRO_SENSOR_PIN = 0;  // Input signal for gyro value (analog pin 0 different from digital pin 0!)
 int altimeter_val = 0;  //Declare variable: measured altimeter value in code.  Initialized at 0
 int front_sensor_val = 0; //Declare variable: measured front sensor value in code.  Initialized at 0
 int gyro_sensor_val = 0; //Declare variable: measured gyro value in code.  Initialized at 0
@@ -23,30 +20,21 @@ int basespeed = (motor1basespeed + motor2basespeed) / 2; // This is the average 
 // before altitude correction.
 int helispeed = basespeed; // basespeed with altitude correction.
 int trim = (motor1basespeed - motor2basespeed) / 2;
-int helispeed7 = motor1basespeed;
-int helispeed8 = motor2basespeed;
-int normhelispeed7 = motor1basespeed;
-int normhelispeed8 = motor2basespeed;
+
 long VCCmV = 3500; // current averaged battery voltage in millivolts.
 int rudder = 0;  //Declare variable:
 
 int yaw_rate = 0;
 int AltGoal = 120; //#Set by user: roughly in cm to determine free flight height off ground
 int AltErr; //Declared: Difference between current altimeter_val and AltGoal
-int scan = 0;  //Declared: Used for RH_LH Scan of Front Sensor
-int scanDir = 1; //Declared: Used for RH_LH Scan of Front Sensor
-int scan2 = 0; //Declared: Used for RH_LH Scan of Front Sensor
-int scan2Dir = 1; //Declared: Used for RH_LH Scan of Front Sensor
 int counter = 0; //Declared: Used for looping exercises within the code
 
 void setup()  {
-  //pinMode(ALTIMETER_PIN, INPUT); //sets the pin as input for downward sensor
-  //pinMode(FRONT_SENSOR_PIN, INPUT); //sets the pin as input for front sensor
-  //pinMode(GYRO_SENSOR_PIN, INPUT); //sets the pin as input for onboard gyro sensor
-  // initialize the digital pin as an output.
+  // initialize the digital output pins.
   pinMode(led, OUTPUT);    // sets the pin as output as RED led out
   pinMode(motor1, OUTPUT);   // sets the pin as output
   pinMode(motor2, OUTPUT);   // sets the pin as output
+  // The analog input pins don't need to be initialized.
   VCCmV = readVcc();
 }
 
@@ -102,7 +90,6 @@ void loop()  {
   rampMotors(0, basespeed, 50);
   int base_gyro_val = analogRead(GYRO_SENSOR_PIN);
   int gyro_target = base_gyro_val;
-  int real_gyro_target = base_gyro_val;
 
   delay(100);
 
@@ -125,44 +112,16 @@ void loop()  {
 
     helispeed = basespeed + AltErr;
 
-    int cycle1Pos = counter % 1000;
-    if ( cycle1Pos > 499 ) {
-      scanDir = -1;
-    } else {
-      scanDir = 1;
-    }
-    // cycle2Pos goes from 0 to 400, then back down to 0. And repeats.
-    // cycle2 is a different length than cycle1 so the behavior is more interesting.
-    int cycle2Pos = counter % 800;
-    if ( cycle2Pos > 400 ) {
-      cycle2Pos = 800 - cycle2Pos;
-    }
-    // Let's make this a nice -100 to +100 value.
-    int cycle2Norm = (cycle2Pos - 200) / 2;
     if (front_sensor_val < 100)
     {
       digitalWrite(led, HIGH);
-      scanDir = 1;
-      // digitalWrite(5, HIGH);
-      //gyro_target = base_gyro_val + scanDir * ((50- front_sensor_val) / 5);
       gyro_target = base_gyro_val - 100;
     }
     else
     {
       digitalWrite(led, LOW);
-      cycle2Norm = 0;
-      gyro_target = base_gyro_val + (cycle2Norm / 50);
+      gyro_target = base_gyro_val;
     }
-
-    // This next bit of code limits the slew rate, or rate of change.
-    if ( gyro_target > real_gyro_target ) {
-      real_gyro_target = real_gyro_target + 1;
-    } else {
-      real_gyro_target = real_gyro_target - 1;
-    }
-    //yaw_rate = (((base_gyro_val+rudder+scan/2) - gyro_sensor_val));
-    //yaw_rate = (gyro_sensor_val - real_gyro_target);
-//    yaw_rate = ( real_gyro_target - gyro_sensor_val );
     yaw_rate = ( gyro_target - gyro_sensor_val );
     if ( yaw_rate > 30 ) {
       yaw_rate = 30;
@@ -170,12 +129,11 @@ void loop()  {
       yaw_rate = -30;
     }
     setMotors(helispeed, yaw_rate );
-    // setMotors(helispeed, 0);
 
     counter = counter + 1;
     if (counter > 3000) {
-      rampMotors(helispeed, helispeed / 2, 300);
-      analogWrite(motor1, 0);
+      rampMotors(helispeed, helispeed / 2, 300); // Gently land.
+      analogWrite(motor1, 0); // Turn the motors all the way off.
       analogWrite(motor2, 0);
       delay(10000);
       counter = 0;
@@ -185,6 +143,5 @@ void loop()  {
       analogWrite(motor2, 0);
       delay(10000);
     }
-    //delay(200);
   }
 }
